edb3b8c9a8495d3a5d319e9a841102c77ada8287
PROFF-1 <richmondnyamedor04@gmail.com>
2025-04-02 15:26:19 +0000

    Bottom Tab Nsvigation Established

--- ExpensesApp/App.js
+++ ExpensesApp/App.js
@@ -1,14 +1,60 @@
-import { StyleSheet, Text, View,StatusBar,SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View,StatusBar,SafeAreaView,Image } from 'react-native';
 import {styles} from './Styles/styles'
 import Home from "./Screens/Home"
 import Settings from "./Screens/Settings"
+import MyCards from "./Screens/MyCards"
+import Statistics from "./Screens/Statistics"
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Settings/>
-      <StatusBar  style='auto'/>
-    </View>
+    <NavigationContainer style={styles.container}>
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={Home}
+        options={
+          {
+            tabBarIcon:({color,size})=>(
+              <Image source={require('./assets/home.png')}
+              style={{ width: 20, height: 20, tintColor: color }}
+              />
+            )
+          }
+        }/>
+        <Tab.Screen name ='My Card' component={MyCards}
+         options={
+          {
+            tabBarIcon:({color,size})=>(
+              <Image source={require('./assets/myCards.png')}
+              style={{ width: 20, height: 20, tintColor: color }}
+              />
+            )
+          }}
+          />
+        <Tab.Screen  name='Statistics' component={Statistics}
+        options={
+          {
+            tabBarIcon:({color,size})=>(
+              <Image source={require('./assets/statictics.png')}
+              style={{ width: 20, height: 20, tintColor: color }}
+              />
+            )
+          }}/>
+        <Tab.Screen name ='Settings' component={Settings}
+        options={
+          {
+            tabBarIcon:({color})=>(
+              <Image source={require('./assets/settings.png')}
+              style={{ width: 20, height: 20, tintColor: color }}
+              />
+            )
+          }}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
